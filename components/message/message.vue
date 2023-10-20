2277f1f4335336645b6628716a5d9f95612458b7
yuelingfan <103427935+yuelingfan@users.noreply.github.com>
2023-10-20 17:55:52 +0800

    Add files via upload

---
+++ components/message/message.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class='card' @click='toSystemNotification()'>
+		<view class='top'>
+			<text>{{title}}</text>
+		</view>
+		<view class='content'>
+			<up-text size='12' :text='content' :lines='3'></up-text>
+		</view>
+		<view  class='bottom'>
+			<view class='d_left'>
+				<up-text size='12' type='primary' text='查看详情'></up-text>
+			</view>
+			<view class='d_right'>
+				<up-text size='12' type="info" :text="time"></up-text>
+			</view>
+			<view class='clear'></view>
+		</view>
+		<u-line color="#ddd"></u-line>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"message",
+		data() {
+			return {
+				title:'袁神通知',
+				content:'您好,您已经被袁神盯上了，小心生命安全!tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt',
+				time:'10天前',
+			};
+		},
+		methods:{
+			toSystemNotification(){
+				uni.navigateTo({
+					url:"/pages/Message/MessageBox/SystemNotification"
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+.card{
+	margin: 40rpx 55rpx;
+}
+.top{
+	margin-bottom: 30rpx;
+	font-size: 40rpx;
+	font-weight: 550;
+}
+.d_left{
+	float: left;
+}
+.d_right{
+	float: right;
+}
+.bottom{
+	margin: 20rpx 0;
+}
+.clear{
+	clear: both;
+}
+</style>
---
+++ components/topic/essence_topic.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class='card' @click='toPost()'>
+		<view class="margin title">
+			<text>{{title}}</text>
+		</view>
+		<view class='content'>
+			<view v-for='(item,index) in content'>
+				<view class='margin d_left'>
+					<u-tag :icon='item[0]' :text="item[1]" shape="circle" bgColor='#ddddddbb' borderColor='#ddddddbb'></u-tag>
+				</view>
+				<view class='clear'></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"essential_post",
+		data() {
+			return {
+				userName:'yuelingfan',
+				title:'蜗的鹊桥 | 真·表白墙',
+				content:[
+					['https://cdn.uviewui.com/uview/example/tag.png','tttttttttttttt'],
+					['','tttttttttttttttttt'],
+					['','ttttttttttt'],
+				]
+			};
+		},
+		methods:{
+			toPost(){
+				uni.navigateTo({
+					url:"/pages/Square/Post/Post"
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+.card{
+	padding: 30rpx;
+	margin: 30rpx;
+	color: #fff;
+	background: linear-gradient(#4bb1ef, #ffe1c2);
+	border-radius: 20rpx;
+}
+.title{
+	font-size: 40rpx;
+}
+.d_left{
+	float: left;
+}
+.d_right{
+	float: right;
+}
+.margin{
+	margin: 15rpx;
+}
+.content{
+	margin: 20rpx 0;
+	padding-top: 10rpx;
+}
+.clear{
+	clear: both;
+}
+</style>
